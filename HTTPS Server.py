227091509f73027632e0bc35b376c05899c3e950
hexxednight <hexxednight@gmail.com>
2024-10-13 18:26:24 +0200

    v1

---
+++ HTTPS Server.py
@@ -0,0 +1,11 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+
+class SylHTTP(BaseHTTPRequestHandler):
+    def do_get(self)
+    self.send_response(200)
+    self.send_header("content-type", "text/html")
+    self.end:headers
+
+    self.wfile.write(bytes(SylHTTP))
+
